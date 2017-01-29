cc8ece3dc05c3fcd1695712df205cba4f9855c0b
venkatra <venkatesh.sekar@gmail.com>
2017-01-29 08:37:56 -0500

    Added git ignore and tested out dock_fbeats script

--- ws.learn.docker/fbeats_docker/dock_fbeat.sh
+++ ws.learn.docker/fbeats_docker/dock_fbeat.sh
@@ -1,18 +1,18 @@
-WS_DIR=/Users/d3vl0p3r/Dev/workspace/ws.learn.docker/fbeats_docker
+WS_DIR=$(pwd)
 CONTAINER_NAME=fbtdk
 DOCKER_IMAGE_NAME=testdock
 FILEBEATS_DIST=filebeat-5.1.2-linux-x86_64
 
 # https://www.elastic.co/guide/en/beats/filebeat/current/filebeat-installation.html
 
-#echo "Downloading filebeats ${FILEBEATS_DIST} ...."
-#curl -L -O "https://artifacts.elastic.co/downloads/beats/filebeat/${FILEBEATS_DIST}.tar.gz"
-#tar xzvf "${FILEBEATS_DIST}.tar.gz"
-#mv $FILEBEATS_DIST fbeats
+echo "Downloading filebeats ${FILEBEATS_DIST} ...."
+curl -L -O "https://artifacts.elastic.co/downloads/beats/filebeat/${FILEBEATS_DIST}.tar.gz"
+tar xzvf "${FILEBEATS_DIST}.tar.gz"
+mv $FILEBEATS_DIST fbeats
 
-#echo "Building docker image $DOCKER_IMAGE_NAME ..."
-#docker rmi -f $DOCKER_IMAGE_NAME
-#docker build -t $DOCKER_IMAGE_NAME -f Dockerfile .
+echo "Building docker image $DOCKER_IMAGE_NAME ..."
+docker rmi -f $DOCKER_IMAGE_NAME
+docker build -t $DOCKER_IMAGE_NAME -f Dockerfile .
 
 echo "executing docker $CONTAINER_NAME ..."
 EXISTING_CONTAINER=$(docker ps -aqf "name=$CONTAINER_NAME")
