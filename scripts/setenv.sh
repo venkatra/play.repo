50f040f23b0f9406e46873fac0f53bc618bc3e04
venkatra <venkatesh.sekar@gmail.com>
2017-02-06 06:14:09 -0500

    - Defined the base code for wikilinkapp spark based code

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -8,3 +8,7 @@
 WS_DIR=$(pwd)
 
 alias gcsdk='docker run --rm -ti --volumes-from gcloud-config -v ${WS_DIR}:/gcp_learn google/cloud-sdk'
+
+MAVEN_HOME=/Users/d3vl0p3r/Dev/lib/apache-maven-3.3.1/
+
+export PATH=$PATH:${MAVEN_HOME}/bin
---
+++ spark_dataproc/src/main/scala/ca/effacious/learn/gcp/spark/wikilink/AppBase.scala
@@ -0,0 +1,145 @@
+package ca.effacious.learn.gcp.spark
+
+
+import java.io.{File, FileNotFoundException}
+import java.util.Date
+
+import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.commons.lang.RandomStringUtils
+import org.apache.commons.lang.time.DateFormatUtils
+import org.apache.commons.lang3.StringUtils
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+import org.slf4j.{LoggerFactory, MDC}
+
+/**
+  * Thrown in case a command line argument is missing
+  *
+  * @param comandLineArg
+  */
+case class MissingCommandLineArgException(comandLineArg:String)  extends Exception(comandLineArg)
+
+case class ConfigurationNotFoundException(configuration:String)  extends Exception(configuration)
+
+/**
+  * Base class for application jobs
+  */
+trait AppBase {
+  lazy val LOGR = LoggerFactory.getLogger(getClass.getName)
+
+  lazy val APP_NAME = get_app_name() //'DUMMY
+
+  /**
+    * This is a random uniquely generated key that is used to link all log records related to a particular run of
+    * the program.
+    */
+  lazy val RUN_KEY = {
+    val runLogKey = RandomStringUtils.randomAlphabetic(3) + DateFormatUtils.format(new Date, "yyyyMMdd")
+    MDC.put("MDC$RUN_LOG_KEY", runLogKey)
+    runLogKey
+  }
+
+  /**
+    * The scheduler task name under which this task is running as
+    */
+  lazy val c_scheduler_task_name = {
+    parseCommandLineArguments("-scheduler_task_name", c_CommandLineArgsList
+      , "argument [scheduler_task_name] is missing; this is name of the scheduler task, that is executing this program.")
+  }
+
+  var c_sc: SparkContext = _
+  var c_sqlContext: SQLContext = _
+
+  /**
+    * Command line argument
+    */
+  var c_CommandLineArgsList :Seq[String] = _
+
+  /**
+    * Generic application configuration file
+    */
+  var c_appConf :Config = _
+
+
+  def initialize(a_commandLineArgsList :Seq[String]):Unit = {
+    LOGR.info("Initializing ...")
+    c_CommandLineArgsList = a_commandLineArgsList
+
+    val appEnvironmentalConfigFilePath = {parseCommandLineArguments("-appEnvConf", c_CommandLineArgsList
+      ,"argument [appEnvConf] is missing; this is the configuration file containing all the environment specific related settings.", true)}
+
+    val appEnvConf = ConfigFactory.parseFile(new java.io.File(appEnvironmentalConfigFilePath))
+
+    /*
+    * The following provides a way to override the default : application_conf
+    * URL : https://typesafehub.github.io/config/latest/api/com/typesafe/config/Config.html
+    *
+    *
+    */
+    c_appConf = ConfigFactory.load().withFallback(appEnvConf).resolve()
+
+  }
+
+  /**
+    * Used for retrieving a specific parameter from the command line arguments
+    *
+    * @param a_configurationParam
+    * @param a_argsList
+    * @return
+    */
+  def parseCommandLineArguments(a_configurationParam: String, a_argsList: Seq[String], message: String = "argument not present"
+                                , throwException: Boolean = false, defaultVal: String = null): String = {
+
+
+    if ((a_argsList == null) && StringUtils.isNotBlank(defaultVal))
+      return defaultVal
+
+    if(a_argsList.contains(a_configurationParam) == false) {
+      if(throwException)
+        throw new MissingCommandLineArgException(message)
+
+      LOGR.info(message)
+      return defaultVal
+    }
+
+    a_argsList match {
+      case x :: value :: tail if (x == a_configurationParam) =>  value
+
+      case unknownConfig :: value :: tail =>
+        LOGR.trace(s"ignoring current config ${unknownConfig}, when checking for ${a_configurationParam} ");
+        parseCommandLineArguments(a_configurationParam,tail)
+    }
+
+  }
+
+  def getSparkConf(a_appName: String): SparkConf = {
+    new SparkConf(true).setAppName(a_appName).setMaster("local[2]").set("spark.executor.memory","1g")
+  }
+
+  /**
+    * Sets the spark contexts
+    *
+    */
+  def initializeSparkContext(a_appName: String ) = {
+    if (LOGR.isInfoEnabled)
+      LOGR.info("Initializing spark context ...")
+
+    val sparkConf = getSparkConf(a_appName)
+    c_sc = new SparkContext(sparkConf)
+    c_sqlContext = new org.apache.spark.sql.SQLContext(c_sc)
+  }
+
+  def stopSparkContext(): Unit = {
+    if (LOGR.isInfoEnabled())
+      LOGR.info("Stopping spark context ....")
+
+    c_sc.stop()
+  }
+
+  def setContextObjects(a_sc: SparkContext, a_sqlContext: SQLContext): Unit = {
+    c_sc = a_sc
+    c_sqlContext = a_sqlContext
+  }
+
+  def get_app_name():String
+}
